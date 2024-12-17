3b7c88b54c153816d9bc6743c87ab0057463886f
tero51 <akmalmagistr@gmail.com>
2024-12-18 00:53:59 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,78 @@
+import random
+
+class Animal:
+    live = True
+    sound = None
+    _DEGREE_OF_DANGER = 0
+
+    def __init__(self, speed):
+        self._cords = [0, 0, 0]
+        self.speed = speed
+
+    def move(self, dx, dy, dz):
+        new_z = self._cords[2] + dz * self.speed
+        if new_z < 0:
+            print("It's too deep, i can't dive :(")
+        else:
+            self._cords[0] += dx * self.speed
+            self._cords[1] += dy * self.speed
+            self._cords[2] = new_z
+
+    def get_cords(self):
+        print(f"X: {self._cords[0]} Y: {self._cords[1]} Z: {self._cords[2]}")
+
+    def attack(self):
+        if self._DEGREE_OF_DANGER < 5:
+            print("Sorry, i'm peaceful :)")
+        else:
+            print("Be careful, i'm attacking you 0_0")
+
+    def speak(self):
+        if self.sound:
+            print(self.sound)
+        else:
+            print("I don't make any sound.")
+
+
+class Bird(Animal):
+    beak = True
+
+    def lay_eggs(self):
+        eggs_count = random.randint(1, 4)
+        print(f"Here are(is) {eggs_count} eggs for you")
+
+
+class AquaticAnimal(Animal):
+    _DEGREE_OF_DANGER = 3
+
+    def dive_in(self, dz):
+        dz = abs(dz)
+        new_z = self._cords[2] - dz * (self.speed / 2)
+        if new_z < 0:
+            print("It's too deep, i can't dive :(")
+        else:
+            self._cords[2] = new_z
+
+class PoisonousAnimal(Animal):
+    _DEGREE_OF_DANGER = 8
+
+class Duckbill(Bird, AquaticAnimal, PoisonousAnimal):
+    sound = "Click-click-click"
+
+# Пример работы программы
+if __name__ == "__main__":
+    db = Duckbill(10)
+
+    print(db.live)
+    print(db.beak)
+
+    db.speak()
+    db.attack()
+
+    db.move(1, 2, 3)
+    db.get_cords()
+
+    db.dive_in(6)
+    db.get_cords()
+
+    db.lay_eggs()
